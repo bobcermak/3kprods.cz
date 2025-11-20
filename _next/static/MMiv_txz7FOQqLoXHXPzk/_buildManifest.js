f6399e96188e6f0f9a3888608020987f3d295ce3
bobcermak <bohuslavcermak62@gmail.com>
2025-11-20 21:21:07 +0100

    Updates

--- _next/static/MMiv_txz7FOQqLoXHXPzk/_buildManifest.js
+++ _next/static/MMiv_txz7FOQqLoXHXPzk/_buildManifest.js
@@ -1,12 +1,7 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [
-      {
-        "source": "/3kprods.cz//_next/:path+",
-        "destination": "/3kprods.cz/_next/:path+"
-      }
-    ],
+    "beforeFiles": [],
     "fallback": []
   },
   "sortedPages": [
