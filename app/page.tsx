ba0ea31eb744c2d763c400364020797e7de3032d
bobcermak <bohuslavcermak62@gmail.com>
2025-11-24 12:09:59 +0100

    Refactor Home component layout; add header and footer sections, update Button styles for better spacing, and ensure newline at end of package-lock.json

--- app/page.tsx
+++ app/page.tsx
@@ -4,13 +4,15 @@ import { H1, H2, H3, H4, P, Ul, Li, Button } from '@/components';
 const Home = () => {
   return (
     <>
-      {/* <header></header> */}
-      <main className='bg-light-blue'>
-        <H1>Welcome to 3kprods.cz</H1>
-        <Image src="/images/content/couple-image.webp" alt="Ukázkový obrázek" width={800} height={600} sizes="(max-width: 768px) 100vw, 50vw"/>
-        <Image src="/images/content/prom-image.webp" alt="Ukázkový obrázek" width={800} height={600} sizes="(max-width: 768px) 100vw, 50vw"/>
+      <header>
+
+      </header>
+      <main>
+        <Button>Click me</Button>
       </main>
-      {/* <footer></footer> */}
+      <footer>
+
+      </footer>
     </>
   )
 }
--- components/buttons/Button.tsx
+++ components/buttons/Button.tsx
@@ -13,7 +13,7 @@ type ButtonProps = {
   children: React.ReactNode;
 }
 const Button: FC<ButtonProps> = ({ isLink = true, link = "#", variant = "primary", isArrow = true, onClick, children }) => {
-  const baseStyles: string = `inline-block px-6 py-3 no-underline rounded-[2.5rem] text-body font-bold text-center uppercase cursor-pointer w-fit backdrop-blur-xl backdrop-saturate-150 border border-white/20 transition-colors duration-250 ease-in-out ${isArrow ? "group" : ""}`;
+  const baseStyles: string = `inline-block pl-6 ${isArrow ? "pr-3" : "pr-6"} py-3 no-underline rounded-[2.5rem] text-body font-bold text-center uppercase cursor-pointer w-fit backdrop-blur-xl backdrop-saturate-150 border border-white/20 transition-colors duration-250 ease-in-out ${isArrow ? "group" : ""}`;
   const variantStyles: Record<"primary" | "secondary", string> = {
     primary: `bg-[#4461EF]/50 text-white hover:bg-[#4461EF]/70 hover:shadow-sm`,
     secondary: `bg-[#C7EFB3]/50 text-blue hover:bg-[#C7EFB3]/70 hover:shadow-sm`
@@ -21,7 +21,7 @@ const Button: FC<ButtonProps> = ({ isLink = true, link = "#", variant = "primary
   const combinedClasses: string = twMerge(baseStyles, variantStyles[variant]);
   const external: boolean = link ? link.startsWith("http") : false;
   const arrow = isArrow ? (
-    <i className={`fa-solid fa-arrow-right ml-[.625rem] rounded-full transition-transform duration-250 ease-in-out group-hover:translate-x-[3px] ${variant === "primary" ? "text-white bg-light-blue" : "text-blue bg-green"} py-[0.375rem] px-[0.4375rem]`}></i>
+    <i className={`fa-solid fa-arrow-right ml-2.5 rounded-full transition-transform duration-250 ease-in-out group-hover:translate-x-[3px] ${variant === "primary" ? "text-white bg-light-blue" : "text-blue bg-green"} py-[0.375rem] px-[0.4375rem]`}></i>
   ) : null;
   if (!isLink) return <button className={combinedClasses} onClick={onClick}>{children}{arrow}</button>;
   return external ? (
