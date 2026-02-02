87c64274547bca584f3b5a2f7e1a4c27d370c707
bobcermak <bohuslavcermak62@gmail.com>
2026-02-02 11:32:38 +0100

    fix: enhance logo click behavior for smooth scrolling in Navbar and Footer components

--- components/layout/header/Navbar.tsx
+++ components/layout/header/Navbar.tsx
@@ -24,7 +24,7 @@ const Navbar: FC<NavbarProps> = ({ logo, button, className, href, children }) =>
     }, [isMenuOpen]);
     return (
         <nav className={classes}>
-            <Link href="/" className="w-[80px] h-[80px] z-50" onClick={() => setIsMenuOpen(false)}><Image className="img-responsive" src={logo} alt="3kprods - logo" width={100} height={100}/></Link>
+            <Link href="/" className="w-[80px] h-[80px] z-50" onClick={() => { setIsMenuOpen(false); window.scrollTo({ top: 0, behavior: 'smooth' }); }}><Image className="img-responsive" src={logo} alt="3kprods - logo" width={100} height={100}/></Link>
             <Ul onClick={() => setIsMenuOpen(false)} type="menu" className={`flex laptop:items-center absolute laptop:static top-full laptop:top-none left-0 laptop:left-none right-0 laptop:right-none mx-5 mt-6 laptop:m-0 rounded-[1.25rem] laptop:rounded-b-none shadow-secondary laptop:shadow-none inner-shadow-primary inner-shadow-none laptop:justify-center bg-[linear-gradient(77deg,#1C2E55_0%,#243c91_25%,#4461ef_49%,#98a8f7_100%)] laptop:bg-none flex-col laptop:flex-row gap-6 laptop:gap-8 px-3 py-6 laptop:p-0 transition-all duration-550 ease-in-out laptop:opacity-100 laptop:pointer-events-auto ${isMenuOpen ? "opacity-100 translate-x-0" : "opacity-0 -translate-x-10 pointer-events-none"}`}>
                 {children}
             </Ul>
